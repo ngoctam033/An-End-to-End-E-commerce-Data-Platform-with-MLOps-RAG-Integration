cd23f2cb3e7a6c18aefb0950826aad2218f56343
ngoctam033 <nguyenngoctam0332003@gmail.com>
2026-01-13 20:19:36 +0700

    Add PostgreSQL configuration for database connection

---
+++ gen_data/db_config.py
@@ -0,0 +1,8 @@
+postgres_config = {
+    "host": "localhost",       # Địa chỉ máy chủ (thường là localhost hoặc IP)
+    "port": 5432,              # Cổng mặc định của PostgreSQL là 5432
+    "database": "ecommerce_db", # Tên cơ sở dữ liệu
+    "user": "postgres",    # Tên người dùng database
+    "password": "password", # Mật khẩu
+    "sslmode": "prefer"        # Chế độ SSL (tùy chọn)
+}
